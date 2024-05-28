51b2ea3cfc3a62800d30459bc0b8ed4359ed62a0
Stéphane SADIO <stephane.sadio@efor-group.com>
2024-05-28 16:25:06 +0200

    prepare base model stage completed

--- src/cnnClassifier/entity/__init__.py
+++ src/cnnClassifier/entity/__init__.py
@@ -1,1 +1,5 @@
-from cnnClassifier.entity.config_entity import (DataIngestionConfig)
+from cnnClassifier.entity.config_entity import (DataIngestionConfig, 
+                                                PrepareBaseModelConfig,
+                                                PrepareCallbacksConfig,
+                                                TrainingConfig,
+                                                EvaluationConfig)
